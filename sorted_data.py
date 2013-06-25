1ac191e28e3fca0514f96e782631dfca5f33cf01
riyamodi <rm662@georgetown.edu>
2013-06-24 17:19:38 -0700

    Second Commit: condensed

--- sorted_data.py
+++ sorted_data.py
@@ -4,18 +4,24 @@ import re
 script, filename = argv
 
 text = open(filename)
-filetext = text.read()
+filetext = text.readlines()
 text.close()
 
-filetext = re.sub(":","\n",filetext)
-filelist = filetext.split("\n")
+# filetext = re.sub(":","\n",filetext)
+# filelist = filetext.split("\n")
 
 d = {}
 
 counter = 1
-for item in filelist[:-2:2]:
-    d[item] = d.get(item,filelist[counter])
-    counter += 2
+for line in filetext:
+    name, score = line.strip().split(":")
+    # line_components = line.strip()
+    # line_components = line_components.split(":")
+    # name = line_components[0]
+    # score = int(line_components[1])
+    d[name] = int(score)
+    # d[item] = d.get(item,filelist[counter])
+    # counter += 2
 
 sorted_key =sorted(d.iteritems(), key= lambda x : x[0])
 
